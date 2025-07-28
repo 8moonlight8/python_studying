cc7fbc0a0b03514ac483250fbd61f483b12cf586
8moonlight8 <mohamadoubambandiaye@gmail.com>
2025-07-28 01:29:03 +0000

    first

---
+++ test.py
@@ -0,0 +1,53 @@
+# Test de la fonction print avec end et sep
+print( "Bonjour", "les", "amis", sep="-", end="!\n" )
+print( "Vous allez bien ?")
+
+# Repetirion de chaînes 
+#permet de répéter la chaîne 2 fois
+print("nigga" * 2) 
+
+
+# Affichage d'entier,d'hexadécimal et octal
+print( 42 )  # Affiche l'entier
+print( 0x2A )  # Affiche l'entier en hexadécimal
+print( 0o52 )  # Affiche l'entier en octal
+
+# Affichage de reels ou flottants
+print( 3.14 )  # Affiche le réel
+print( 1e-10 )  # Affiche le réel en notation scientifique
+
+# Affichage de booléens
+print( True > False )  # Hehe lol
+
+# Affichage de None
+print( None )  # Je ne sais pas ce que c'est mais on l'affiche quand même
+
+# Les operateurs de base
+print( 1 + 2 )  # Addition
+print( 5 - 3 )  # Soustraction
+print( 4 * 2 )  # Multiplication
+print( 8 / 2 )  # Division  
+print( 5 // 2 )  # Division entière; il arondit à l'entier de gauche
+print( 5 % 2 )  # Modulo
+print( 2 ** 3 )  # Puissance
+
+# Priorité des opérateurs ;  op-unaires,then **,then \,*,%,then +,-, then <,>,>=,<=, then ==,!=,then and,or
+
+var="Hello World"
+print("ma nigga " + var)  # Concaténation de chaînes
+
+# La fonction input
+
+anything = input("Tell me anything...")
+print("Hmm...", anything, "...Really?")
+
+# La conversion de chaine
+# on a int(),float(),str()
+man = int(input("donne un numero"))
+print("Son carre est ",man**2)
+
+# + peut etre utiliser pour concatener des chaines 
+
+#Operateurs au niveau du bit
+# & | ~ ^
+# Je n'ai pas vraiment compri cette partie la lol
